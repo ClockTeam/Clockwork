946354b7dd6bf6c86a5839bb8b23957ababaff65
cosmy <cosmy_rulezz@hotmail.it>
2016-04-12 20:26:58 +0200

    Detect platform architecture at compile time.

--- Source/Urho3D/Config.h
+++ Source/Urho3D/Config.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2016 Clockwork Engine. All Rights Reserved.
-// Check LICENSE for more informations.
+// See License.txt in the project root for license information.
 
 #pragma once
 
@@ -21,4 +21,25 @@
 #   define CW_COMPILER_GCC (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
 #else
 #   error Unknown compiler
+#endif
+
+/// Detect current architecture
+#if defined(__x86_64__) || defined(_M_X64)
+#   define CW_ARCH_STR "AMD64"
+#   define CW_ARCH_AMD64 1
+#   define CW_ARCH_64BIT 1
+#elif defined(__aarch64__)
+#   define CW_ARCH_STR "ARM64"
+#   define CW_ARCH_ARM64 1
+#   define CW_ARCH_64BIT 1
+#elif defined(__i386) || defined(_M_IX86)
+#   define CW_ARCH_STR "x86"
+#   define CW_ARCH_X86 1
+#   define CW_ARCH_32BIT 1
+#elif defined(__arm__) || defined(_M_ARM)
+#   define CW_ARCH_STR "ARM"
+#   define CW_ARCH_ARM 1
+#   define CW_ARCH_32BIT 1
+#else
+#   error Unknown architecture
 #endif
