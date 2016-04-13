0042ae2d56eb64debb5717371f86cd25fae3faba
dragonCASTjosh <dragonCASTjosh@gmail.com>
2016-04-13 13:29:09 +0100

    Fixed URHO3D_IS_BUILDING remaining code

--- Source/Urho3D/AngelScript/Addons.h
+++ Source/Urho3D/AngelScript/Addons.h
@@ -22,11 +22,9 @@
 
 #pragma once
 
-#ifdef URHO3D_IS_BUILDING
+
 #include "Urho3D.h"
-#else
-#include <Urho3D/Urho3D.h>
-#endif
+
 
 #include "../Container/HashMap.h"
 #include "../Container/Str.h"
