94e34b2dd990a5c9fbd0a4111a83f95d8f4a3ab0
cosmy <cosmy_rulezz@hotmail.it>
2016-04-17 01:38:40 +0200

    Add Civetweb and kNet GENie scripts.

---
+++ scripts/Civetweb.lua
@@ -0,0 +1,20 @@
+-- Copyright (c) 2016 Clockwork Engine. All Rights Reserved.
+-- See License.txt in the project root for license information.
+
+function Civetweb_Project()
+    group "Libs"
+    project "Civetweb"
+        targetname "Civetweb"
+        kind "StaticLib"
+        language "C++"
+        prepareProject("Civetweb", "../lib")
+
+        files { 
+            "../Source/ThirdParty/Civetweb/src/*.*",
+            "../Source/ThirdParty/Civetweb/include/*.*"
+        }
+
+        includedirs { "../Source/ThirdParty/Civetweb/include" }
+
+        vpaths { ["*"] = { "../Source/ThirdParty/Civetweb/src", "../Source/ThirdParty/Civetweb/include" } }
+end
---
+++ scripts/kNet.lua
@@ -0,0 +1,35 @@
+-- Copyright (c) 2016 Clockwork Engine. All Rights Reserved.
+-- See License.txt in the project root for license information.
+
+function kNet_Project()
+    group "Libs"
+    project "kNet"
+        targetname "kNet"
+        kind "StaticLib"
+        language "C++"
+        prepareProject("kNet", "../lib")
+
+        files {
+            "../Source/ThirdParty/kNet/src/*.*",
+            "../Source/ThirdParty/kNet/include/*.*",
+            "../Source/ThirdParty/kNet/include/kNet/*.*",
+        }
+
+        if isWindows() or isDurango() then
+            files {
+                "../Source/ThirdParty/kNet/src/win32/*.*",
+                "../Source/ThirdParty/kNet/include/kNet/win32/*.*",
+            }
+
+            defines {
+                "_WINSOCKAPI_",
+                "KNET_MEMORY_LEAK_CHECK"
+            }
+        else
+            files { "../Source/ThirdParty/kNet/src/unix/*.*" }
+        end
+
+        includedirs { "../Source/ThirdParty/kNet/include/" }
+
+        vpaths { ["*"] = "../Source/ThirdParty/kNet" }
+end
