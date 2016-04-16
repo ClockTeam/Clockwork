be87654557eb3611b53fae92911d44044e406b80
cosmy <cosmy_rulezz@hotmail.it>
2016-04-16 02:32:33 +0200

    Add LZ4 and StanHull GENie scripts.

---
+++ scripts/LZ4.lua
@@ -0,0 +1,15 @@
+-- Copyright (c) 2016 Clockwork Engine. All Rights Reserved.
+-- See License.txt in the project root for license information.
+
+function LZ4_Project()
+    group "Libs"
+    project "LZ4"
+        targetname "LZ4"
+        kind "StaticLib"
+        language "C++"
+        prepareProject("LZ4", "../lib")
+
+        files { "../ThirdParty/LZ4/*.c", "../ThirdParty/LZ4/*.h" }
+
+        vpaths { ["*"] = "../Source/ThirdParty/LZ4/" }
+end
