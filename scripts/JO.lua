4bacf4ca31e7d506bb1ec70c57134a36995bc556
cosmy <cosmy_rulezz@hotmail.it>
2016-04-16 23:28:44 +0200

    ADD jo_jpeg GENie script.

---
+++ scripts/JO.lua
@@ -0,0 +1,15 @@
+-- Copyright (c) 2016 Clockwork Engine. All Rights Reserved.
+-- See License.txt in the project root for license information.
+
+function JO_Project()
+    group "Libs"
+    project "JO"
+        targetname "JO"
+        kind "StaticLib"
+        language "C++"
+        prepareProject("JO", "../lib")
+
+        files { "../Source/ThirdParty/JO/*.*" }
+
+        vpaths { ["*"] = "../Source/ThirdParty/JO/" }
+end
